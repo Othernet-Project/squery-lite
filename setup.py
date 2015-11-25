943286a397889876b8033fa73b5b368d3618753e
Andrean Franc <andrean.franc@gmail.com>
2015-11-25 17:28:53 +0100

    Update classifiers

--- setup.py
+++ setup.py
@@ -24,12 +24,12 @@ setup(
     ],
     classifiers=[
         'Development Status :: 3 - Alpha',
-        'Topic :: Applicaton',
-        'Framework :: Bottle',
+        'Topic :: Library',
         'Environment :: Web Environment',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
     ],
 )
