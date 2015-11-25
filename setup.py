60255bb04c33d6aa4b2b3408e010d999b483f864
Andrean Franc <andrean.franc@gmail.com>
2015-11-25 17:27:15 +0100

    Update setup.py module

--- setup.py
+++ setup.py
@@ -1,18 +1,15 @@
-#!/usr/bin/env python
-
 import os
-from setuptools import setup
+from setuptools import setup, find_packages
 
 import squery_lite as pkg
-
-
-VERSION = pkg.__version__
 
 
 def read(fname):
     """ Return content of specified file """
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
+
+VERSION = pkg.__version__
 
 setup(
     name='squery-lite',
@@ -21,14 +18,18 @@ setup(
     packages=[pkg.__name__],
     include_package_data=True,
     long_description=read('README.rst'),
+    install_requires=[
+        'pytz',
+        'sqlize',
+    ],
     classifiers=[
-        'Development Status :: 1 - Pre Alpha',
+        'Development Status :: 3 - Alpha',
+        'Topic :: Applicaton',
+        'Framework :: Bottle',
+        'Environment :: Web Environment',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
-    ],
-    install_requires=[
-        'sqlize',
-        'pytz',
     ],
 )
