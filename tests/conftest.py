92cf3f232b642c3d6bfe2b9c5a398bfe02225b0d
Branko Vukelic <branko@outernet.is>
2016-08-17 12:21:28 +0200

    Fix fixture organization
    Signed-off-by: Branko Vukelic <branko@outernet.is>

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,13 @@
 """
-This module contains classes and functions useful when testing an application
-using squery-pg.
+This module contains fixtures for use with py.test test runner.
 """
 
 import os
 import hashlib
 
-from .squery import Database
+import pytest
+
+from squery_lite.squery import Database
 
 
 def random_name(prefix='test'):
@@ -120,3 +121,34 @@ class TestContainer(object):
             return self.databases[name]['db']
         except KeyError:
             return object.__getattr__(self, name)
+
+
+@pytest.fixture(scope='session')
+def database_container(request, database_config):
+    """
+    A test database container with automatic tear-down. This fixture requires
+    you to define a ``database_config`` session-scoped fixture that returns a
+    dict containing keyword arguments for the :py:class:`TestContainer` class.
+
+    To speed the tests up, no set-up is done.
+
+    This is a session-scoped fixture and tear-down is performed after all tests
+    are done.
+    """
+    container = TestContainer(**database_config)
+
+    def teardown():
+        container.teardownall()
+    request.addfinalizer(teardown)
+    return container
+
+
+@pytest.fixture
+def databases(database_container):
+    """
+    Database container with setup performed for all databases. You should
+    create your own fixtures that depend on the :py:func:`database_container`
+    fixture if you wish to avoid running the setup on all tables.
+    """
+    database_container.setupall()
+    return database_container
